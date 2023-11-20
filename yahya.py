c73b18328601c34d1f40d1c9a8a1502706fc543a
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-11-20 09:02:03 +0100

    Update yahya.py

--- yahya.py
+++ yahya.py
@@ -1,28 +1,23 @@
-def remplir_matrice(N, P):
-    if N <= 0 or P <= 0 or N > 10 or P > 10:
-        print("Les valeurs de N et P doivent être > 0 et ≤ 10.")
-        return
+package company;
+class clsEmployer
+{
+    public String FullName;
+    public String ID;
+    public String Job;
 
-    matrice = [[0] * P for i in range(N)]
+    public clsEmployer(String FullName, String ID, String Job)
+    {
+        this.FullName = FullName;
+        this.ID = ID;
+        this.Job = Job;
+    }
+    public void showInfo()
+    {
 
-    valeur = 1
-    for i in range(N):
-        for j in range(P):
-            matrice[i][j] = valeur
-            valeur += 1
+        System.out.println("Employer Name : " + this.FullName);
+        System.out.println("\nEmployer ID :" + this.ID);
+        System.out.println("\nEmployer Job : " + this.Job);
 
-    print("Affichage normal:")
-    for i in range(N):
-        print(matrice[i])
+    }
 
-    print("\nAffichage alternatif:")
-    for i in range(N):
-        if i % 2 == 0:
-            print(matrice[i])
-        else:
-            print(matrice[i][::-1])
-
-
-N = int(input("Saisire l' entier N :"))
-P = int(input("Saisire l' entier P :"))
-remplir_matrice(N, P)
+}
