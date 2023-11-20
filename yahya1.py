871031fb301f48c6cd21edb86c6556330f402ef1
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-11-20 09:03:08 +0100

    Update yahya1.py

--- yahya1.py
+++ yahya1.py
@@ -1,20 +1,18 @@
+package animals;
 
-for i in range(1,4):
-    non_client=input("entre le nom du client :")
-    y = int(input("svp done le nobre darticle:"))
-    t=0
-    for j in range(1,y+1):
-        prix= int(input("svp done le prix des articles:"))
-        tva=(prix*0.15)
-        ttc=((prix+tva)-((prix+tva)*0.02))
-        print("le prix sons taxe",prix,"le tva",tva)
-        s =ttc
-        print("le prix total ",s)
-        t+=s
-    print("le prix total est  de tout les article: ", t)
+class clsAnimal
+  {
+    String name;
+    String FootNumber;
 
+    public clsAnimal(String name, String FootNumber)
+    {
+        this.name = name;
+        this.FootNumber = FootNumber;
+    }
+    public void showAnimal()
+    {
+        System.out.println("the animal " + this.name + " has : " + this.FootNumber + " Foot." );
+    }
 
-
-
-
-
+  }
