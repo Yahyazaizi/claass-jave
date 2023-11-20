3cdf7ba263bbfe910979bcdbd7ae3d6898c19d8b
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-11-20 11:52:13 +0100

    Update yahya1.py

--- yahya1.py
+++ yahya1.py
@@ -5,7 +5,7 @@ class clsSPORT
     String name;
     String playernumber;
 
-    public cls(String name, String playernumber)
+    public clsSPORT(String name, String playernumber)
     {
         this.name = name;
         this.playernumber = playernumber;
